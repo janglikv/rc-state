0e6d1bee834cd026a4b605119f30cf8d8582c7d5
jianglikui <jianglikui@ezone.work>
2024-06-06 20:04:58 +0800

    Add default hook useData and allow custom method.

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,11 @@ export function createStore<
     State,
     Selector extends Record<string, (state: State) => any>,
     Actions extends Record<string, (state: State, ...payload: any) => State>,
->({ state, selector, actions }: { state?: State; selector?: Selector; actions?: Actions }) {
+    Others extends Record<string, any> = {},
+>({ state, selector, actions, useData, ...others }: { state?: State; selector?: Selector; useData?: Function, actions?: Actions } & Others) {
     actions = actions || ({} as any);
     selector = selector || ({} as any);
-    const StoreContext = createContext<any>({ state, uuid: 0, callbacks: {}, actions });
+    const StoreContext = createContext<any>({ state, uuid: 0, callbacks: {}, actions, setState() { } });
 
     function StateKeeper({ initState }: any) {
         const context = useContext(StoreContext);
@@ -31,6 +32,9 @@ export function createStore<
             context.state = state.current;
             const keys = Object.keys(context.callbacks);
             keys.forEach((uuid) => {
+                if (!context.callbacks[uuid]) {
+                    return
+                }
                 const [selector, prevValue, updater] = context.callbacks[uuid];
                 const newValue = selector(state.current);
                 newValue !== prevValue && updater();
@@ -55,26 +59,30 @@ export function createStore<
         return null;
     }
 
+    const AutoUse = ({ useData }: { useData: Function }) => {
+        useData();
+        return null;
+    }
+
     const Provider = ({ children, initState }: { children: any; initState?: State | ((state?: State) => State) }) => {
         const value = useMemo(() => ({ callbacks: {}, uuid: 0, state: {}, actions: {} }), []);
         initState = initState instanceof Function ? initState(state) : initState;
         initState = initState ?? state;
         return (
             <StoreContext.Provider value={value}>
+                {(typeof useData === 'function') && <AutoUse useData={useData as Function} />}
                 <StateKeeper initState={initState} />
                 {children}
             </StoreContext.Provider>
         );
     };
 
-    const useSelector = <S extends (state: State) => any>(selector: S) => {
+    const useSelector = <S extends (state: State) => ReturnType<S>>(selector: S) => {
         const context = useContext(StoreContext);
         const [, setKey] = useState(false);
         const uuid = useMemo(() => context.uuid++, []);
         const callbackRef = useRef<any>();
-        callbackRef.current = useMemo(() => {
-            return [selector, selector(context.state), () => setKey((bool) => !bool)];
-        }, [selector, uuid]);
+        callbackRef.current = useMemo(() => [selector, selector(context.state), () => setKey((bool) => !bool)], [selector, uuid]);
         context.callbacks[uuid] = callbackRef.current;
 
         useEffect(() => {
@@ -88,9 +96,7 @@ export function createStore<
     };
 
     type RestParameters<T> = T extends (first: any, ...rest: infer R) => any ? R : never;
-    type ActionFunctions = {
-        [K in keyof Actions]: (...args: RestParameters<Actions[K]>) => void;
-    };
+    type ActionFunctions = { [K in keyof Actions]: (...args: RestParameters<Actions[K]>) => void; };
     const useActions = () => {
         const context = useContext(StoreContext);
         const newActions: ActionFunctions = context.actions as ActionFunctions;
@@ -109,10 +115,6 @@ export function createStore<
         (useSelectors as any)[key] = () => useSelector(selectorFunc);
     });
 
-    const useSelectorsList = Object.keys(useSelectors || {}).map((key) => {
-        return (useSelectors as any)[key];
-    });
-
     type UseStoreContext = {
         setState: React.Dispatch<React.SetStateAction<State>>;
         getState: () => State;
@@ -127,39 +129,37 @@ export function createStore<
         }, []);
     };
 
+    const StoreContextConsumer = ({ children }: { children: any }) => {
+        const ctx = useStoreContext()
+        return children(ctx)
+    }
+
     return {
         Provider,
         state: useSelectors,
         useSelector,
         useActions,
         useContext: useStoreContext,
+        StoreContextConsumer,
         withProvider: <T,>(Component: T, initState?: State | ((state?: State) => State)): T => {
             return ((props: any) => {
                 const EComponent = Component as unknown as ComponentType<any>;
                 return (
                     <Provider initState={initState}>
-                        <EComponent {...props} />
+                        <StoreContextConsumer>
+                            {(storeContext: any) => <EComponent {...props} storeContext={storeContext} />}
+                        </StoreContextConsumer>
                     </Provider>
                 );
             }) as unknown as T;
         },
-        Consumer: ({
-            selectors = [],
-            children,
-        }: {
-            selectors?: Array<(state: State) => any | UseSelectors>;
-            children?: ({ }: { values: any[]; actions: ActionFunctions; context: UseStoreContext }) => any;
-        }) => {
-            const values = selectors.map((selector) => {
-                if (useSelectorsList.includes(selector)) {
-                    const useValue = selector as any;
-                    return useValue();
-                }
-                return useSelector(selector);
-            });
-            const actions = useActions();
-            const context = useStoreContext();
-            return children?.({ values, actions, context }) ?? null;
+        withContext: <T,>(Component: T): T => {
+            return ((props: any) => {
+                const storeContext = useStoreContext();
+                const EComponent = Component as unknown as ComponentType<any>;
+                return <EComponent {...props} storeContext={storeContext} />;
+            }) as unknown as T;
         },
+        ...others
     };
 }
